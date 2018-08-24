b75c2c49bc2fef3c6ddfa86d885b7b7d79838631
kyle <kyle.a.zhou@gmail.com>
2018-08-23 23:21:13 -0500

    suny

--- service.py
+++ service.py
@@ -10,5 +10,10 @@ def root():
 def endpoint1():
     return "endpoint1"
 
+@app.route("/sunnyis/", methods=['GET'])
+def sunny():
+    return "not fat"
+
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port=5005)
